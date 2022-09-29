dec48ac78560d365790e2d4207423b6e302d1ba7
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-09-30 01:00:30 +0600

    btn-added

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -6,9 +6,16 @@ const Exercise = (props) => {
     return (
         <div className='exercise'>
             <img src={img} alt="" />
-            <h3>Name: {name}</h3>
-            <p>{description}</p>
-            <h4>Time: {Time} </h4>
+            <div className='exercise-info'>
+                <p>Name: {name}</p>
+                <p>{description}</p>
+                <p><h4>Time: {Time}</h4> </p>
+            </div>
+            <div>
+                <button className='btn-cart'>
+                    <h5>Add to list</h5>
+                </button>
+            </div>
 
 
         </div>
