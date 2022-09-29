03adfdda88572782960472fdd69f5ea2a6ff4f4d
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-09-29 22:03:43 +0600

    data-load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Exercise from './components/Exercise/Exercise';
 import Header from './components/Header/Header';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header></Header>
+      <Exercise></Exercise>
     </div>
   );
 }
